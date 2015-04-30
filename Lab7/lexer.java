2a89eabf60aa7672759630ba1917f67ddc12c318
MahmoudGamalEid <mahmoudgamaleid@gmail.com>
2015-04-30 18:34:44 +0200

    la la balash

---
+++ Lab7/lexer.java
@@ -0,0 +1,760 @@
+import java.util.*;
+import java.io.*;
+/* semantic value of token returned by scanner */
+public class lexer
+{
+	public static void main (String[] args) throws IOException
+	{
+		BufferedReader reader = new BufferedReader(new FileReader("input.hs"));
+		BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+		Yylex yy = new Yylex (reader);
+		while(true)
+		{
+			String x =yy.next_token();
+			if(x==null)
+				break;
+			writer.write(x);	
+			writer.write('\n');
+		}
+		reader.close();
+		writer.close();
+	}
+}
+
+
+class Yylex {
+	private final int YY_BUFFER_SIZE = 512;
+	private final int YY_F = -1;
+	private final int YY_NO_STATE = -1;
+	private final int YY_NOT_ACCEPT = 0;
+	private final int YY_START = 1;
+	private final int YY_END = 2;
+	private final int YY_NO_ANCHOR = 4;
+	private final int YY_BOL = 128;
+	private final int YY_EOF = 129;
+
+  public String sourceFilename;
+	private java.io.BufferedReader yy_reader;
+	private int yy_buffer_index;
+	private int yy_buffer_read;
+	private int yy_buffer_start;
+	private int yy_buffer_end;
+	private char yy_buffer[];
+	private boolean yy_at_bol;
+	private int yy_lexical_state;
+
+	Yylex (java.io.Reader reader) {
+		this ();
+		if (null == reader) {
+			throw (new Error("Error: Bad input stream initializer."));
+		}
+		yy_reader = new java.io.BufferedReader(reader);
+	}
+
+	Yylex (java.io.InputStream instream) {
+		this ();
+		if (null == instream) {
+			throw (new Error("Error: Bad input stream initializer."));
+		}
+		yy_reader = new java.io.BufferedReader(new java.io.InputStreamReader(instream));
+	}
+
+	private Yylex () {
+		yy_buffer = new char[YY_BUFFER_SIZE];
+		yy_buffer_read = 0;
+		yy_buffer_index = 0;
+		yy_buffer_start = 0;
+		yy_buffer_end = 0;
+		yy_at_bol = true;
+		yy_lexical_state = YYINITIAL;
+	}
+
+	private boolean yy_eof_done = false;
+	private final int BCOMMENT = 2;
+	private final int YYINITIAL = 0;
+	private final int COMMENT = 1;
+	private final int yy_state_dtrans[] = {
+		0,
+		43,
+		46
+	};
+	private void yybegin (int state) {
+		yy_lexical_state = state;
+	}
+	private int yy_advance ()
+		throws java.io.IOException {
+		int next_read;
+		int i;
+		int j;
+
+		if (yy_buffer_index < yy_buffer_read) {
+			return yy_buffer[yy_buffer_index++];
+		}
+
+		if (0 != yy_buffer_start) {
+			i = yy_buffer_start;
+			j = 0;
+			while (i < yy_buffer_read) {
+				yy_buffer[j] = yy_buffer[i];
+				++i;
+				++j;
+			}
+			yy_buffer_end = yy_buffer_end - yy_buffer_start;
+			yy_buffer_start = 0;
+			yy_buffer_read = j;
+			yy_buffer_index = j;
+			next_read = yy_reader.read(yy_buffer,
+					yy_buffer_read,
+					yy_buffer.length - yy_buffer_read);
+			if (-1 == next_read) {
+				return YY_EOF;
+			}
+			yy_buffer_read = yy_buffer_read + next_read;
+		}
+
+		while (yy_buffer_index >= yy_buffer_read) {
+			if (yy_buffer_index >= yy_buffer.length) {
+				yy_buffer = yy_double(yy_buffer);
+			}
+			next_read = yy_reader.read(yy_buffer,
+					yy_buffer_read,
+					yy_buffer.length - yy_buffer_read);
+			if (-1 == next_read) {
+				return YY_EOF;
+			}
+			yy_buffer_read = yy_buffer_read + next_read;
+		}
+		return yy_buffer[yy_buffer_index++];
+	}
+	private void yy_move_end () {
+		if (yy_buffer_end > yy_buffer_start &&
+		    '\n' == yy_buffer[yy_buffer_end-1])
+			yy_buffer_end--;
+		if (yy_buffer_end > yy_buffer_start &&
+		    '\r' == yy_buffer[yy_buffer_end-1])
+			yy_buffer_end--;
+	}
+	private boolean yy_last_was_cr=false;
+	private void yy_mark_start () {
+		yy_buffer_start = yy_buffer_index;
+	}
+	private void yy_mark_end () {
+		yy_buffer_end = yy_buffer_index;
+	}
+	private void yy_to_mark () {
+		yy_buffer_index = yy_buffer_end;
+		yy_at_bol = (yy_buffer_end > yy_buffer_start) &&
+		            ('\r' == yy_buffer[yy_buffer_end-1] ||
+		             '\n' == yy_buffer[yy_buffer_end-1] ||
+		             2028/*LS*/ == yy_buffer[yy_buffer_end-1] ||
+		             2029/*PS*/ == yy_buffer[yy_buffer_end-1]);
+	}
+	private java.lang.String yytext () {
+		return (new java.lang.String(yy_buffer,
+			yy_buffer_start,
+			yy_buffer_end - yy_buffer_start));
+	}
+	private int yylength () {
+		return yy_buffer_end - yy_buffer_start;
+	}
+	private char[] yy_double (char buf[]) {
+		int i;
+		char newbuf[];
+		newbuf = new char[2*buf.length];
+		for (i = 0; i < buf.length; ++i) {
+			newbuf[i] = buf[i];
+		}
+		return newbuf;
+	}
+	private final int YY_E_INTERNAL = 0;
+	private final int YY_E_MATCH = 1;
+	private java.lang.String yy_error_string[] = {
+		"Error: Internal error.\n",
+		"Error: Unmatched input.\n"
+	};
+	private void yy_error (int code,boolean fatal) {
+		java.lang.System.out.print(yy_error_string[code]);
+		java.lang.System.out.flush();
+		if (fatal) {
+			throw new Error("Fatal Error.\n");
+		}
+	}
+	private int[][] unpackFromString(int size1, int size2, String st) {
+		int colonIndex = -1;
+		String lengthString;
+		int sequenceLength = 0;
+		int sequenceInteger = 0;
+
+		int commaIndex;
+		String workString;
+
+		int res[][] = new int[size1][size2];
+		for (int i= 0; i < size1; i++) {
+			for (int j= 0; j < size2; j++) {
+				if (sequenceLength != 0) {
+					res[i][j] = sequenceInteger;
+					sequenceLength--;
+					continue;
+				}
+				commaIndex = st.indexOf(',');
+				workString = (commaIndex==-1) ? st :
+					st.substring(0, commaIndex);
+				st = st.substring(commaIndex+1);
+				colonIndex = workString.indexOf(':');
+				if (colonIndex == -1) {
+					res[i][j]=Integer.parseInt(workString);
+					continue;
+				}
+				lengthString =
+					workString.substring(colonIndex+1);
+				sequenceLength=Integer.parseInt(lengthString);
+				workString=workString.substring(0,colonIndex);
+				sequenceInteger=Integer.parseInt(workString);
+				res[i][j] = sequenceInteger;
+				sequenceLength--;
+			}
+		}
+		return res;
+	}
+	private int yy_acpt[] = {
+		/* 0 */ YY_NOT_ACCEPT,
+		/* 1 */ YY_NO_ANCHOR,
+		/* 2 */ YY_NO_ANCHOR,
+		/* 3 */ YY_NO_ANCHOR,
+		/* 4 */ YY_NO_ANCHOR,
+		/* 5 */ YY_NO_ANCHOR,
+		/* 6 */ YY_NO_ANCHOR,
+		/* 7 */ YY_NO_ANCHOR,
+		/* 8 */ YY_NO_ANCHOR,
+		/* 9 */ YY_NO_ANCHOR,
+		/* 10 */ YY_NO_ANCHOR,
+		/* 11 */ YY_NO_ANCHOR,
+		/* 12 */ YY_NO_ANCHOR,
+		/* 13 */ YY_NO_ANCHOR,
+		/* 14 */ YY_NO_ANCHOR,
+		/* 15 */ YY_NO_ANCHOR,
+		/* 16 */ YY_NO_ANCHOR,
+		/* 17 */ YY_NO_ANCHOR,
+		/* 18 */ YY_NO_ANCHOR,
+		/* 19 */ YY_NO_ANCHOR,
+		/* 20 */ YY_NO_ANCHOR,
+		/* 21 */ YY_NO_ANCHOR,
+		/* 22 */ YY_NO_ANCHOR,
+		/* 23 */ YY_NO_ANCHOR,
+		/* 24 */ YY_NO_ANCHOR,
+		/* 25 */ YY_NO_ANCHOR,
+		/* 26 */ YY_NO_ANCHOR,
+		/* 27 */ YY_NO_ANCHOR,
+		/* 28 */ YY_NO_ANCHOR,
+		/* 29 */ YY_NO_ANCHOR,
+		/* 30 */ YY_NO_ANCHOR,
+		/* 31 */ YY_NO_ANCHOR,
+		/* 32 */ YY_NO_ANCHOR,
+		/* 33 */ YY_NO_ANCHOR,
+		/* 34 */ YY_NO_ANCHOR,
+		/* 35 */ YY_NO_ANCHOR,
+		/* 36 */ YY_NO_ANCHOR,
+		/* 37 */ YY_NO_ANCHOR,
+		/* 38 */ YY_NO_ANCHOR,
+		/* 39 */ YY_NOT_ACCEPT,
+		/* 40 */ YY_NO_ANCHOR,
+		/* 41 */ YY_NO_ANCHOR,
+		/* 42 */ YY_NO_ANCHOR,
+		/* 43 */ YY_NOT_ACCEPT,
+		/* 44 */ YY_NO_ANCHOR,
+		/* 45 */ YY_NO_ANCHOR,
+		/* 46 */ YY_NOT_ACCEPT,
+		/* 47 */ YY_NO_ANCHOR,
+		/* 48 */ YY_NO_ANCHOR,
+		/* 49 */ YY_NO_ANCHOR,
+		/* 50 */ YY_NO_ANCHOR,
+		/* 51 */ YY_NO_ANCHOR,
+		/* 52 */ YY_NO_ANCHOR,
+		/* 53 */ YY_NO_ANCHOR,
+		/* 54 */ YY_NO_ANCHOR,
+		/* 55 */ YY_NO_ANCHOR,
+		/* 56 */ YY_NO_ANCHOR,
+		/* 57 */ YY_NO_ANCHOR,
+		/* 58 */ YY_NO_ANCHOR,
+		/* 59 */ YY_NO_ANCHOR,
+		/* 60 */ YY_NO_ANCHOR,
+		/* 61 */ YY_NO_ANCHOR,
+		/* 62 */ YY_NO_ANCHOR,
+		/* 63 */ YY_NO_ANCHOR,
+		/* 64 */ YY_NO_ANCHOR,
+		/* 65 */ YY_NO_ANCHOR,
+		/* 66 */ YY_NO_ANCHOR,
+		/* 67 */ YY_NO_ANCHOR,
+		/* 68 */ YY_NO_ANCHOR
+	};
+	private int yy_cmap[] = unpackFromString(1,130,
+"11:8,45:2,3,11,45,5,11:18,45,12,14,11:3,7,11,44,43,13,20,19,1,10,11,39:10,1" +
+"5,40,17,9,16,6,11,38:26,41,11,42,11,18,11,22,38:2,21,26,25,37,30,24,38:2,27" +
+",38,36,29,34,38,31,28,23,38,35,32,38,33,38,4,8,2,11:2,0:2")[0];
+
+	private int yy_rmap[] = unpackFromString(1,69,
+"0,1,2,1,3,1,4,5,6,1,7,8,9,1:2,10,11,1:13,12,1:3,13,1:4,12,1,14,15,16,1,17,1" +
+"8,19,20,12,21,22,23,13,24,25,26,27,28,29,30,31,32,33,34,13,35,36,37")[0];
+
+	private int yy_nxt[][] = unpackFromString(38,46,
+"1,2,3,4,40,4,5,6,7,8,41,44,47,9,49,10,11,12,13,14,15,16,65,66,42,65,67,65:2" +
+",68,65:9,17,18,19,20,21,22,4,-1:47,23,-1:14,24,-1:22,17,-1:9,4,-1,4,-1:39,4" +
+",-1:7,25,-1:46,26,-1:46,27,-1:51,31,-1:39,32,-1:45,33,-1:56,15,-1:46,65,51," +
+"65:3,52,65:12,53,-1:7,39:2,-1,39,-1,39:8,30,39:31,-1:21,65:18,53,-1:7,28:2," +
+"-1,28,-1,28:40,-1:21,65:4,34,65:13,53,-1:6,1,35:2,36,35:42,-1:21,65,34,65:1" +
+"6,53,-1:6,1,37:2,38:43,-1:12,29,-1:54,65:5,34,65:12,53,-1:27,65:16,34,65,53" +
+",-1:27,65:2,45,65:15,53,-1:27,65:10,57,65:7,53,-1:27,65:13,48,65:4,53,-1:27" +
+",65:7,48,65:10,53,-1:27,65:9,58,65:8,53,-1:27,65:3,59,65:14,53,-1:27,65:5,6" +
+"0,65:12,53,-1:27,65:14,61,65:3,53,-1:27,65:10,62,65:7,53,-1:27,65:3,63,65:1" +
+"4,53,-1:27,65:11,64,65:6,53,-1:27,65:15,50,65:2,53,-1:27,65:3,55,65:14,53,-" +
+"1:27,65:12,54,65:5,53,-1:27,65:6,55,65:11,53,-1:27,65:2,56,65:15,53,-1:6");
+
+	public String next_token ()
+		throws java.io.IOException {
+		int yy_lookahead;
+		int yy_anchor = YY_NO_ANCHOR;
+		int yy_state = yy_state_dtrans[yy_lexical_state];
+		int yy_next_state = YY_NO_STATE;
+		int yy_last_accept_state = YY_NO_STATE;
+		boolean yy_initial = true;
+		int yy_this_accept;
+
+		yy_mark_start();
+		yy_this_accept = yy_acpt[yy_state];
+		if (YY_NOT_ACCEPT != yy_this_accept) {
+			yy_last_accept_state = yy_state;
+			yy_mark_end();
+		}
+		while (true) {
+			if (yy_initial && yy_at_bol) yy_lookahead = YY_BOL;
+			else yy_lookahead = yy_advance();
+			yy_next_state = YY_F;
+			yy_next_state = yy_nxt[yy_rmap[yy_state]][yy_cmap[yy_lookahead]];
+			if (YY_EOF == yy_lookahead && true == yy_initial) {
+
+  return null;
+			}
+			if (YY_F != yy_next_state) {
+				yy_state = yy_next_state;
+				yy_initial = false;
+				yy_this_accept = yy_acpt[yy_state];
+				if (YY_NOT_ACCEPT != yy_this_accept) {
+					yy_last_accept_state = yy_state;
+					yy_mark_end();
+				}
+			}
+			else {
+				if (YY_NO_STATE == yy_last_accept_state) {
+					throw (new Error("Lexical Error: Unmatched Input."));
+				}
+				else {
+					yy_anchor = yy_acpt[yy_last_accept_state];
+					if (0 != (YY_END & yy_anchor)) {
+						yy_move_end();
+					}
+					yy_to_mark();
+					switch (yy_last_accept_state) {
+					case 1:
+						
+					case -2:
+						break;
+					case 2:
+						{
+  yybegin(BCOMMENT);
+}
+					case -3:
+						break;
+					case 3:
+						{ 
+  return "RC\t"+ yytext();
+}
+					case -4:
+						break;
+					case 4:
+						{ 
+}
+					case -5:
+						break;
+					case 5:
+						{
+  return "QM\t" + yytext();
+}
+					case -6:
+						break;
+					case 6:
+						{ 
+  return "Undefined\t"+yytext(); 
+}
+					case -7:
+						break;
+					case 7:
+						{ 
+  return "BR\t"+ yytext();
+}
+					case -8:
+						break;
+					case 8:
+						{ 
+  return "AO\t="; 
+}
+					case -9:
+						break;
+					case 9:
+						{ 
+  return "MB\t"+ yytext();
+}
+					case -10:
+						break;
+					case 10:
+						{ 
+  return "CL\t"+ yytext();
+}
+					case -11:
+						break;
+					case 11:
+						{ 
+  return "GT\t"+ yytext();
+}
+					case -12:
+						break;
+					case 12:
+						{ 
+  return "LT\t"+ yytext();
+}
+					case -13:
+						break;
+					case 13:
+						{ 
+  return "WC\t"+ yytext();
+}
+					case -14:
+						break;
+					case 14:
+						{ 
+  return "FA\t"+ yytext();
+}
+					case -15:
+						break;
+					case 15:
+						{ 
+  return "CN\t"+ yytext();
+}
+					case -16:
+						break;
+					case 16:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -17:
+						break;
+					case 17:
+						{ 
+  return "NM\t"+ yytext();
+}
+					case -18:
+						break;
+					case 18:
+						{ 
+  return "SM\t"+ yytext();
+}
+					case -19:
+						break;
+					case 19:
+						{ 
+  return "LS\t"+ yytext();
+}
+					case -20:
+						break;
+					case 20:
+						{ 
+  return "RS\t"+ yytext();
+}
+					case -21:
+						break;
+					case 21:
+						{ 
+  return "RB\t"+ yytext();
+}
+					case -22:
+						break;
+					case 22:
+						{ 
+  return "LB\t"+ yytext();
+}
+					case -23:
+						break;
+					case 23:
+						{
+  yybegin(COMMENT);
+}
+					case -24:
+						break;
+					case 24:
+						{ 
+  return "AR\t"+ yytext();
+}
+					case -25:
+						break;
+					case 25:
+						{
+  return "AA\t" + yytext();
+}
+					case -26:
+						break;
+					case 26:
+						{
+  return "OO\t" + yytext();
+}
+					case -27:
+						break;
+					case 27:
+						{
+  return "EQ\t" + yytext();
+}
+					case -28:
+						break;
+					case 28:
+						{ 
+  return "DD\t"+ yytext();
+}
+					case -29:
+						break;
+					case 29:
+						{ 
+  return "FN\t"+ yytext();
+}
+					case -30:
+						break;
+					case 30:
+						{ 
+  return "ST\t"+ yytext();
+}
+					case -31:
+						break;
+					case 31:
+						{ 
+  return "HT\t"+ yytext();
+}
+					case -32:
+						break;
+					case 32:
+						{ 
+  return "GE\t"+ yytext();
+}
+					case -33:
+						break;
+					case 33:
+						{ 
+  return "LE\t"+ yytext();
+}
+					case -34:
+						break;
+					case 34:
+						{ 
+  return "KW\t"+ yytext();
+}
+					case -35:
+						break;
+					case 35:
+						{
+}
+					case -36:
+						break;
+					case 36:
+						{
+  yybegin(YYINITIAL);
+}
+					case -37:
+						break;
+					case 37:
+						{
+  yybegin(YYINITIAL);
+}
+					case -38:
+						break;
+					case 38:
+						{
+}
+					case -39:
+						break;
+					case 40:
+						{
+  yybegin(BCOMMENT);
+}
+					case -40:
+						break;
+					case 41:
+						{ 
+  return "Undefined\t"+yytext(); 
+}
+					case -41:
+						break;
+					case 42:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -42:
+						break;
+					case 44:
+						{ 
+  return "Undefined\t"+yytext(); 
+}
+					case -43:
+						break;
+					case 45:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -44:
+						break;
+					case 47:
+						{ 
+  return "Undefined\t"+yytext(); 
+}
+					case -45:
+						break;
+					case 48:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -46:
+						break;
+					case 49:
+						{ 
+  return "Undefined\t"+yytext(); 
+}
+					case -47:
+						break;
+					case 50:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -48:
+						break;
+					case 51:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -49:
+						break;
+					case 52:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -50:
+						break;
+					case 53:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -51:
+						break;
+					case 54:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -52:
+						break;
+					case 55:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -53:
+						break;
+					case 56:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -54:
+						break;
+					case 57:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -55:
+						break;
+					case 58:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -56:
+						break;
+					case 59:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -57:
+						break;
+					case 60:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -58:
+						break;
+					case 61:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -59:
+						break;
+					case 62:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -60:
+						break;
+					case 63:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -61:
+						break;
+					case 64:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -62:
+						break;
+					case 65:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -63:
+						break;
+					case 66:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -64:
+						break;
+					case 67:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -65:
+						break;
+					case 68:
+						{ 
+  return "ID\t"+ yytext();
+}
+					case -66:
+						break;
+					default:
+						yy_error(YY_E_INTERNAL,false);
+					case -1:
+					}
+					yy_initial = true;
+					yy_state = yy_state_dtrans[yy_lexical_state];
+					yy_next_state = YY_NO_STATE;
+					yy_last_accept_state = YY_NO_STATE;
+					yy_mark_start();
+					yy_this_accept = yy_acpt[yy_state];
+					if (YY_NOT_ACCEPT != yy_this_accept) {
+						yy_last_accept_state = yy_state;
+						yy_mark_end();
+					}
+				}
+			}
+		}
+	}
+}
